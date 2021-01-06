cc253c0b1fc7a9131cf01af2e4a4573a950ee3b7
unknown <marcusdimitri7@gmail.com>
2021-01-06 19:47:28 -0300

    Criação do Projeto ReactJS

---
+++ front-web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Hello from './Hello';
+
+function App() {
+  return (
+
+    <div>
+      <Hello></Hello>
+    </div>
+  );
+}
+
+export default App;
