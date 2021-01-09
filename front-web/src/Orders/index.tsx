89a18a7ed93549cd661d72b20d608c5f44f3e8bf
unknown <marcusdimitri7@gmail.com>
2021-01-09 02:02:37 -0300

    Implmentação api listagem do produto

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { fetchProducts } from './api';
+import ProductList from './ProductsList';
 import StepsHeaders from './StepsHeaders';
 import './styles.css'
+import { Product } from './types';
 
 function Orders() {
+    const [products, setProduct] = useState<Product[]>([]);
+    console.log(products)
+    useEffect(() => {
+        fetchProducts()
+        .then(response => setProduct(response.data))
+        .catch(error => console.log(error))
+              
+     },[] );
     return (
         <div className="orders-container">
            <StepsHeaders/>
+           <ProductList products={products}/>
         </div>
   
     )
